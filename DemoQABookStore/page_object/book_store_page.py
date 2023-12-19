7c0f811fb11fab52cc4f758154331f2099e3b5ed
andy-sotatek <andy.nguyen3@sotatek.com>
2023-12-19 09:45:20 +0700

    update code

--- DemoQABookStore/page_object/book_store_page.py
+++ DemoQABookStore/page_object/book_store_page.py
@@ -32,10 +32,18 @@ class BookStorePage(BasePage):
         self._search_book_text_box.enter(book_name)
     
     def verify_book(self,book_name):
+        # expected_book_link_xpath = '//a[contains(text(),\'%s\')]' % book_name
+        # self.expected_book_locator= Element((By.XPATH, expected_book_link_xpath))
+        # a = sys.getsizeof(self.expected_book_locator)
+        # actual_book_link_xpath = '//span[contains(@id, "see-book")]'
+        # self.actual_book_locator= Element((By.XPATH, actual_book_link_xpath))
+        # b = sys.getsizeof(self.actual_book_locator)
+        # assert a == b
+        
         expected_book_link_xpath = '//a[contains(text(),\'%s\')]' % book_name
-        self.expected_book_locator= Element((By.XPATH, expected_book_link_xpath))
-        a = sys.getsizeof(self.expected_book_locator)
+        self.expected_book_locator= Element.find_elements((By.XPATH, expected_book_link_xpath))
+        a = len(self.expected_book_locator)
         actual_book_link_xpath = '//span[contains(@id, "see-book")]'
-        self.actual_book_locator= Element((By.XPATH, actual_book_link_xpath))
-        b = sys.getsizeof(self.actual_book_locator)
+        self.actual_book_locator= Element.find_elements((By.XPATH, actual_book_link_xpath))
+        b = len(self.actual_book_locator)
         assert a == b
